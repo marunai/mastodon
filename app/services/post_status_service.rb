59f847d7c89ec1f2ed1bc4cb72483d18718a4d82
noellabo <noel.yoshiba@gmail.com>
2020-02-24 11:36:40 +0900

    fix CONFLICT 016fddd

--- app/services/post_status_service.rb
+++ app/services/post_status_service.rb
@@ -27,10 +27,12 @@ class PostStatusService < BaseService
     @text        = @options[:text] || ''
     @in_reply_to = @options[:thread]
     @activitypub_distribution = true
+    @quote_id    = @options[:quote_id]
 
     return idempotency_duplicate if idempotency_given? && idempotency_duplicate?
 
     validate_media!
+    preprocess_quote!
     preprocess_attributes!
 
     if scheduled?
@@ -60,6 +62,13 @@ class PostStatusService < BaseService
     @scheduled_at = nil if scheduled_in_the_past?
   rescue ArgumentError
     raise ActiveRecord::RecordInvalid
+  end
+
+  def preprocess_quote!
+    if @quote_id.present?
+      quote = Status.find(@quote_id)
+      @quote_id = quote.reblog_of_id.to_s if quote.reblog?
+    end
   end
 
   def process_status!
@@ -170,7 +179,7 @@ class PostStatusService < BaseService
       language: language_from_option(@options[:language]) || @account.user&.setting_default_language&.presence || LanguageDetector.instance.detect(@text, @account),
       application: @options[:application],
       rate_limit: @options[:with_rate_limit],
-      quote_id: @options[:quote_id],
+      quote_id: @quote_id,
     }.compact
   end
 
