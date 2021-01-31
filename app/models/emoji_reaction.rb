19bb6e6131c06188acde505bbab9d8d066d87cab
marunai <mastodev@worst-friends-chat-mastodon-dev.asia-northeast1-b.c.worst-friends-chat.internal>
2021-01-31 02:35:52 +0000

    ultra-super-commit models/emoji_reaction.rb

--- app/models/emoji_reaction.rb
+++ app/models/emoji_reaction.rb
@@ -25,5 +25,6 @@ class EmojiReaction < ApplicationRecord
 
   def queue_publish
     PushUpdateWorker.perform_async(status.account.id, status_id) unless status.destroyed?
+    PushUpdateWorker.perform_async(status.account.id, status_id) unless status.destroyed?
   end
 end
